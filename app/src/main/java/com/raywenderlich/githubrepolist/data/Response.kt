55760a4b0b53c745d5f3fb138dfd99df17c23e7c
William Simiao <williamsimiao@gmail.com>
2019-05-20 18:10:40 -0300

    migrating to MovieDB

--- app/src/main/java/com/raywenderlich/githubrepolist/data/Response.kt
+++ app/src/main/java/com/raywenderlich/githubrepolist/data/Response.kt
@@ -1,14 +1,30 @@
 package com.raywenderlich.githubrepolist.data
 
-data class RepoResult(val items: List<Item>)
+data class RepoResult(val items: List<movieModel>)
 
-data class Item(
-        val id: Long?,
-        val name: String?,
-        val full_name: String?,
-        val owner: Owner,
-        val private: Boolean,
-        val html_url: String?,
-        val description: String?)
+enum class MovieCodingKeys(val color: String) {
+    ID("id"),
+    POSTER_PATH("poster_path"),
+    BACKDROP_PATH("backdrop_path"),
+    TITLE("title"),
+    RELEASEDATE("release_date"),
+    RATING("vote_average"),
+    OVERVIEW("overview"),
+}
 
-data class Owner(val login: String?, val id: Long?, val avatar_url: String?)
+data class movieModel (
+        val id: Int,
+        val posterPath: String,
+        val backdrop: String,
+        val title: String,
+        val releaseDate: String,
+        val rating: Double,
+        val overview: String)
+
+//        val id: Long?,
+//        val name: String?,
+//        val full_name: String?,
+//        val owner: Owner,
+//        val private: Boolean,
+//        val html_url: String?,
+//        val description: String?)
